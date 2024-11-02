6940d54d7b7def985245db6a735fceb25bdc4058
Fushii <123035475+donfushii@users.noreply.github.com>
2024-11-02 00:50:42 -0300

    Update Imperium.lua

--- Imperium.lua
+++ Imperium.lua
@@ -1,16 +1,16 @@
    -- [ ZOMBIE ATTACK ] --
 if game.PlaceId == 1240123653 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/donfushii/Roblox-Things/main/Scripts/Zombie%20Attack.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Imperium-Development/Imperium/main/Games/Zombie%20Attack.lua"))()
 
    -- [ NATURAL DISASTERS ] --
 elseif game.PlaceId == 189707 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/donfushii/Roblox-Things/main/Scripts/Natural%20Disasters.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Imperium-Development/Imperium/main/Games/Natural%20Disasters.lua"))()
 
    -- [ WORK AT PIZZA PLACE ] --
 elseif game.PlaceId == 192800 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/donfushii/Roblox-Things/main/Scripts/Work%20At%20Pizza%20Place.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Imperium-Development/Imperium/main/Games/Work%20At%20Pizza%20Place.lua"))()
    
    -- [ LEGENDS OF SPEED ] --
 elseif game.PlaceId == 3101667897 or game.PlaceId == 3232996272 or game.PlaceId == 3276265788 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/donfushii/Roblox-Things/main/Scripts/Legends%20Of%20Speed.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Imperium-Development/Imperium/main/Games/Legends%20Of%20Speed.lua"))()
 end
