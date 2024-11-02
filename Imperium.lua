0e6ac72ad8dd5f595e9d0687bcd7762bfd740824
Fushii <123035475+donfushii@users.noreply.github.com>
2024-11-02 00:21:16 -0300

    Create Imperium.lua

---
+++ Imperium.lua
@@ -0,0 +1,16 @@
+   -- [ ZOMBIE ATTACK ] --
+if game.PlaceId == 1240123653 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/donfushii/Roblox-Things/main/Scripts/Zombie%20Attack.lua"))()
+
+   -- [ NATURAL DISASTERS ] --
+elseif game.PlaceId == 189707 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/donfushii/Roblox-Things/main/Scripts/Natural%20Disasters.lua"))()
+
+   -- [ WORK AT PIZZA PLACE ] --
+elseif game.PlaceId == 192800 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/donfushii/Roblox-Things/main/Scripts/Work%20At%20Pizza%20Place.lua"))()
+   
+   -- [ LEGENDS OF SPEED ] --
+elseif game.PlaceId == 3101667897 or game.PlaceId == 3232996272 or game.PlaceId == 3276265788 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/donfushii/Roblox-Things/main/Scripts/Legends%20Of%20Speed.lua"))()
+end
